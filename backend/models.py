6dc460215d6ed309404ed0d53ee3a7c74d6e12ad
harshit <harshit.awasthi.549@gmail.com>
2026-05-02 12:35:48 +0530

    added database setup and models

---
+++ backend/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    role = Column(String, default="member")
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    created_by = Column(Integer, ForeignKey("users.id"))
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    status = Column(String, default="todo")
+    assigned_to = Column(Integer, ForeignKey("users.id"))
+    project_id = Column(Integer, ForeignKey("projects.id"))
