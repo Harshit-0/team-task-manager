f7eb6c44dec689feb863e531949bd23fab1a5792
harshit <harshit.awasthi.549@gmail.com>
2026-05-02 22:09:33 +0530

    fixed authenticaton error

--- backend/main.py
+++ backend/main.py
@@ -54,8 +54,11 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
 
 @app.get("/me")
 def get_me(current_user: models.User = Depends(get_current_user)):
-    return current_user
-
+    return {
+        "id": current_user.id,
+        "email": current_user.email,
+        "role": current_user.role
+    }
 
 # ---------------- PROJECT ----------------
 
